c7cb2393d55603faa61463944a73914a6bf6bc94
ChiragSareen7 <chiragsareen8054@gmail.com>
2024-04-29 12:23:33 +0530

    added signup success animation

--- Front/src/App.jsx
+++ Front/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import SignUp from './Components/SignUp';
 import Open from './Components/Open.jsx';
+import './App.css'
 
 function App() {
   return (
