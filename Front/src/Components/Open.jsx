911e784bec98213ec73a5a4a101bef6030bef1e0
Chirag Sareen <chiragsareen8054@gmail.com>
2024-05-09 17:26:19 +0530

    updated form

--- Front/src/Components/Open.jsx
+++ Front/src/Components/Open.jsx
@@ -1,33 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import Form from './Form';
 
 function Open() {
   const navigate = useNavigate();
+  const [visibility, setVisibility] = useState('none');
 
-  const handleBookAppointment = () => {
-    navigate('/book');
+  const toggleVisibility = () => {
+    setVisibility(visibility === 'none' ? 'flex' : 'none');
   };
 
   return (
     <div className='main'>
       <div className='nav'>
-           <div className='logo-img'>
-                <img src='../src/assets/logo.png'  alt='Logo'/>
-                <p>E-patient</p>
-           </div>
-           <div className='options'>
-              <ul className='list'>
-                  <li>Doctors</li>
-                  <li>Prescription</li>
-                  <li>Profile</li>
-              </ul>
-              <div>
-                  <button onClick={handleBookAppointment}>Book Appointment</button>
-              </div>
-           </div>
-      </div>
-      <div className='poster'>
-        <img src='../src/assets/back.jpg'  alt='doc-patient' className='doc-patient' />
+        <div className='logo-img'>
+          <img src='../src/assets/logo.png' alt='Logo' />
+          <p>E-patient</p>
+        </div>
+        <div className='options'>
+          <ul className='list'>
+            <li>Doctors</li>
+            <li>Prescription</li>
+            <li>Profile</li>
+          </ul>
+          <div>
+            <button onClick={toggleVisibility} className='appoint'>
+              Book Appointment
+            </button>
+          </div>
+          <Form visibility={visibility}/>
+        </div>
       </div>
     </div>
   );
