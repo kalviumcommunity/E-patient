bbe63efb30f39250d81dc5019b6a6233bef63d7f
Chirag Sareen <chiragsareen8054@gmail.com>
2024-05-22 17:50:32 +0530

    Worked on vedio confrencing

---
+++ Front/src/Components/Vedio.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+function Vedio() {
+
+    return(
+        <div>
+            <div>
+            <input 
+                    className='data'
+                    type='text'
+                    name='Name'
+                    placeholder='Name'
+                />
+                <input 
+                    className='data'
+                    type='text'
+                    name='email'
+                    placeholder='email'
+                />
+            </div>
+        </div>
+    )
+}
+export default Vedio;
